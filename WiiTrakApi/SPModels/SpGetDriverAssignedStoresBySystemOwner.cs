cda3c7708fd15832233560a346b20763d35f0a57
SivaSatheesh.MN <sivasatheesh.mn@dsrc.co.in>
2022-03-31 19:03:43 +0530

    Commit :
    Added the SpGetDriverAssignedStoresBySystemOwner

---
+++ WiiTrakApi/SPModels/SpGetDriverAssignedStoresBySystemOwner.cs
@@ -0,0 +1,30 @@
+﻿namespace WiiTrakApi.SPModels
+{
+    public class SpGetDriverAssignedStoresBySystemOwner
+    {
+        public Guid Id { get; set; }
+        public string StoreName { get; set; } = string.Empty;
+
+        public string StoreNumber { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhonePrimary { get; set; } = string.Empty;
+
+        public string PhoneSecondary { get; set; } = string.Empty;
+
+        public string StreetAddress1 { get; set; } = string.Empty;
+
+        public string StreetAddress2 { get; set; } = string.Empty;
+
+        public string City { get; set; } = string.Empty;
+
+        public string State { get; set; } = string.Empty;
+
+        public string CountryCode { get; set; } = string.Empty;
+
+        public string PostalCode { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; }
+    }
+}
