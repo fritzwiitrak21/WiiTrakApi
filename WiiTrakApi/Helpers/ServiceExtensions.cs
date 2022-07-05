9ef8ad7917d78dd9d590819dda7bb93164898674
SivaSatheesh.MN <sivasatheesh.mn@dsrc.co.in>
2022-07-05 20:46:49 +0530

    Staging Commit:
    Cors error

--- WiiTrakApi/Helpers/ServiceExtensions.cs
+++ WiiTrakApi/Helpers/ServiceExtensions.cs
@@ -30,9 +30,9 @@ namespace WiiTrakApi.Helpers
                  builder => builder
                  .SetIsOriginAllowedToAllowWildcardSubdomains()
                  //.SetIsOriginAllowed((host) => true)
-                    .WithOrigins("https://black-hill-07a28510f.1.azurestaticapps.net/",
-                                 "https://wiitrakdemo.com/",
-                                 "https://localhost:7160/")//.WithMethods("GET", "POST", "PUT") // Allow API calls only for these method types.
+                    .WithOrigins("https://black-hill-07a28510f.1.azurestaticapps.net",
+                                 "https://wiitrakdemo.com",
+                                 "https://localhost:7160")//.WithMethods("GET", "POST", "PUT") // Allow API calls only for these method types.
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials() // To allow the credentials for windows authentication
