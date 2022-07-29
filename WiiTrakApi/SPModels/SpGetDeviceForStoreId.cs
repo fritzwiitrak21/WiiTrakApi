33b7d571a0f2e13f67f23d35e15681cfedd54745
SivaSatheesh.MN <sivasatheesh.mn@dsrc.co.in>
2022-07-29 20:49:58 +0530

    Staging Commit:
    Replace the missing SpModel

--- WiiTrakApi/SPModels/SpGetDeviceForStoreId.cs
+++ WiiTrakApi/SPModels/SpGetDeviceForStoreId.cs
@@ -10,8 +10,8 @@ namespace WiiTrakApi.SPModels
         public Guid StoreId { get; set; }
         public string StoreName { get; set; } = string.Empty;
         public string StoreNumber { get; set; } = string.Empty;
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
+        public double StoreLongitude { get; set; }
+        public double StoreLatitude { get; set; }
         public Guid CartId { get; set; }
         public string ManufacturerName { get; set; } = string.Empty;
         public CartStatus Status { get; set; }
@@ -20,5 +20,7 @@ namespace WiiTrakApi.SPModels
         public string DeviceName { get; set; } = string.Empty;
         public string IMEI { get; set; } = string.Empty;
         public string FenceCoords { get; set; } = string.Empty;
+        public double DeviceLongitude { get; set; }
+        public double DeviceLatitude { get; set; }
     }
 }
